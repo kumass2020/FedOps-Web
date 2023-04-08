07e7031f757cba7f3ac7c46a85b5a23eac8507b3
hoho <kumass2021@gmail.com>
2023-04-09 05:16:06 +0900

    add: login page

--- client/src/App.js
+++ client/src/App.js
@@ -1,31 +1,19 @@
 // App.js
 import React from 'react';
-import { AppBar, Toolbar, Typography, Container, Box, Grid } from '@mui/material';
-import Sidebar from './components/common/Sidebar';
+import { Routes,
+  Route,
+} from 'react-router-dom';
+import MainPage from './pages/MainPage';
+import LoginPage from './pages/LoginPage';
+import RegisterPage from './pages/RegisterPage';
 
 const App = () => {
   return (
-    <Box display="flex">
-      <AppBar position="fixed" sx={{ zIndex: (theme) => theme.zIndex.drawer + 1 }}>
-        <Toolbar>
-          <Typography variant="h6">My App</Typography>
-        </Toolbar>
-      </AppBar>
-      <Grid container>
-        <Grid item>
-          <Sidebar />
-        </Grid>
-        <Grid item xs>
-          <Box component="main" flexGrow={1} p={3}>
-            <Toolbar /> {/* This is to keep the main content below the AppBar */}
-            <Container maxWidth="lg">
-              {/* Add your main content here */}
-              <Typography variant="h4">Main Content</Typography>
-            </Container>
-          </Box>
-        </Grid>
-      </Grid>
-    </Box>
+      <Routes>
+        <Route path="/" element={<MainPage/>} />
+        <Route path="/login" element={<LoginPage/>} />
+        <Route path="/register" element={<RegisterPage/>} />
+      </Routes>
   );
 };
 
